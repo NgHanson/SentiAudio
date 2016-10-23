f745561f88385fe655d1725470499593ad55f9d2
chrisexn <cagabour@uwaterloo.ca>
2016-10-23 03:03:01 -0400

    Runnable

--- app/src/main/java/com/microsoft/projectoxford/emotionsample/musicPlayerExample/MusicActivity.java
+++ app/src/main/java/com/microsoft/projectoxford/emotionsample/musicPlayerExample/MusicActivity.java
@@ -4,7 +4,11 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,6 +16,7 @@ import android.widget.Button;
 
 import com.microsoft.projectoxford.emotionsample.R;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +24,15 @@ import com.microsoft.projectoxford.emotionsample.initialization.MusicObject;
 import com.microsoft.projectoxford.emotionsample.initialization.SongListModel;
 import com.microsoft.projectoxford.emotionsample.musicPlayer.PlayerService;
 
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.io.android.AndroidAudioPlayer;
+import be.tarsos.dsp.io.android.AndroidFFMPEGLocator;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
 /**
  * Created by christophE on 2016-10-22.
  */
@@ -37,16 +51,52 @@ public class MusicActivity extends AppCompatActivity{
         mModel = new SongListModel(this);
 
         Button play = (Button)findViewById(R.id.play_song);
-        play.setOnClickListener(new View.OnClickListener() {
+        /*play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(musicSrv!=null)
                     musicSrv.playSong();
             }
-        });
+        });*/
+
+        Build.SUPPORTED_ABIS[0].equals("x86");
+        new com.microsoft.projectoxford.emotionsample.tarsos.AndroidFFMPEGLocator(this);
+        songList.clear();
+        songList.addAll(mModel.getCategoryList(null));
+        final MusicObject obj = songList.get(0);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                File externalStorage = Environment.getExternalStorageDirectory();
+                //File mp3 = new File(externalStorage.getAbsolutePath() , "/audio.mp3");
+                File mp3 =new File(obj.getData());
+                final AudioDispatcher adp;
+
+                adp = AudioDispatcherFactory.fromPipe(mp3.getAbsolutePath(),44100,1024,0);
+                PitchDetectionHandler pitchDetectionHandler = new PitchDetectionHandler() {
+                    @Override
+                    public void handlePitch(PitchDetectionResult pitchDetectionResult, AudioEvent audioEvent) {
+                        float pitch = pitchDetectionResult.getPitch();
+                        double i = audioEvent.getTimeStamp();
+                        double t2 = audioEvent.getEndTimeStamp();
+                        int t = 2;
+                        if(audioEvent.getTimeStamp()>=40){
+                            int j = t;
+                            int n = j;
+                            adp.stop();
+                        }
+                    }
+                };
+                adp.addAudioProcessor(new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN,44100,1024,pitchDetectionHandler));
+                //adp.addAudioProcessor(new AndroidAudioPlayer(adp.getFormat(),5000, AudioManager.STREAM_MUSIC));
+
+                adp.run();
+            }
+        }).start();
     }
 
     //connect to the service
+    /*
     private ServiceConnection musicConnection = new ServiceConnection() {
 
         @Override
@@ -63,5 +113,5 @@ public class MusicActivity extends AppCompatActivity{
         public void onServiceDisconnected(ComponentName name) {
             musicBound = false;
         }
-    };
+    };*/
 }
